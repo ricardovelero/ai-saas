6fe4db05c88034fdd09de5b10590d5e2d9eb1cd6
Ricardo Rodriguez <ricardovelero@gmail.com>
2023-08-17 17:29:09 +0200

    Update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import { ClerkProvider } from "@clerk/nextjs";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Genius",
-  description: "AI Platform",
+  title: "La Furia AI",
+  description: "The best AI Platform",
 };
 
 export default function RootLayout({
