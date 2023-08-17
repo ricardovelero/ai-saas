ba2a0d8e9af01202c7179ced3ae57fdc96121808
Ricardo Rodriguez <ricardovelero@gmail.com>
2023-08-17 17:10:56 +0200

    Install Clerk Authentication

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,5 +1,10 @@
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function DashboardPage() {
-  return <p>Dashboard Page (protected)</p>;
+  return (
+    <div>
+      <p>Dashboard Page (protected)</p>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 }
