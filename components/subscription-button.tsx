9546b02bac77ed72de44a75ebc021ef290f889c9
Ricardo Rodriguez <ricardovelero@gmail.com>
2023-08-19 16:33:07 +0200

    Add settings page w/ subscription button

---
+++ components/subscription-button.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { Zap } from "lucide-react";
+import { Button } from "./ui/button";
+import axios from "axios";
+import { useState } from "react";
+
+interface SubscriptionButtonProps {
+  isPro: boolean;
+}
+
+export default function SubscriptionButton({
+  isPro = false,
+}: SubscriptionButtonProps) {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onClick = async () => {
+    try {
+      setIsLoading(true);
+
+      const response = await axios.get("/api/stripe");
+
+      window.location.href = response.data.url;
+    } catch (error) {
+      console.log("[BILLING_ERROR]", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <Button variant={isPro ? "default" : "premium"} onClick={onClick}>
+      {isPro ? "Manage Subscription" : "Upgrade"}
+      {!isPro && <Zap className="w-4 h-4 ml-2 fill-white" />}
+    </Button>
+  );
+}
