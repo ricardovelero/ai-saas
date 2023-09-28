700ecfbccd97250a8e564aeba6359ad4539a9240
Ricardo Rodriguez <ricardovelero@gmail.com>
2023-09-28 09:19:22 +0200

    Take out crisp momentarily

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <CrispProvider />
+        {/* <CrispProvider /> */}
         <body className={inter.className}>
           <ToasterProvider />
           <ModalProvider />
