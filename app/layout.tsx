78a98d63c3e7ad45c7fa7eb76303606f8245ecfd
Ricardo Rodriguez <ricardovelero@gmail.com>
2023-10-03 11:49:51 +0200

    Takeout Crispr

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,6 @@ import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import ModalProvider from "@/components/modal-provider";
 import ToasterProvider from "@/components/toaster-provider";
-import { CrispProvider } from "@/components/crisp-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,7 +20,6 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        {/* <CrispProvider /> */}
         <body className={inter.className}>
           <ToasterProvider />
           <ModalProvider />
